bd34b2855880c268ea4edd9475ee3ccadbb365a1
whu09 <whu@mines.edu>
2024-06-22 21:20:52 -0600

    moved files

--- ATOEA/ato-ea-site/src/pages/HomePage.tsx
+++
@@ -1,8 +0,0 @@
-function HomePage() {
-    return (
-        <div className="w-screen h-screen bg-blue-200">
-            <div className="text-center">hello</div>
-        </div>
-    );
-}
-export default HomePage
