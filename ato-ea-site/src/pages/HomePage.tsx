1ba34b3e719cf67a554a1f9d4562a965e931a3ab
whu09 <whu@mines.edu>
2024-06-22 21:22:41 -0600

    added files

---
+++ ato-ea-site/src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+function HomePage() {
+    return (
+        <div className="w-screen h-screen bg-blue-200">
+            <div className="text-center">hello</div>
+        </div>
+    );
+}
+export default HomePage
