7d5e06e8e1dbdab1421ba55673af6658d55aacaf
Wesley Hu <whu@mines.edu>
2024-06-25 13:29:23 -0600

    supabase finally working I am actually fucking stupid and didn't enable access to database table and took over 4 hours to figure that out :(

--- ato-ea-site/src/pages/HomePage.tsx
+++ ato-ea-site/src/pages/HomePage.tsx
@@ -1,31 +1,49 @@
 import { useEffect, useState } from "react";
-import { fetchCarouselImages } from "../supabaseClient";
+import { supabase } from '../utils/supabaseClient';
+import { Database } from "../utils/supabaseTypes";
+
+type Images = Database['public']['Tables']['HomePage']['Row'];
 
 function HomePage() {
-    const[imageUrls, setImageUrls] = useState<string[]>([]);
-
-    useEffect(() =>{
-        const fetchData = async() => {
-            const fetchedImageUrls = await fetchCarouselImages();
-            setImageUrls(fetchedImageUrls);
+    const [images, setImages] = useState<Images[]>([]);
+    
+    async function fetchImages() {
+        try {
+            const { data, error } = await supabase
+                .from("HomePage")
+                .select("img_src")
+                .eq("display", true);
+            if (error) {
+                console.error('Error from Supabase:', error);
+                throw error;
+            }
+            setImages(data);
+        } catch (error) {
+            console.error("Error fetching images", error);
         }
-        console.log(imageUrls);
-        fetchData();
-    },[])
+    }
+
+    useEffect(() => {
+        fetchImages();
+    }, []);
+
+    useEffect(() => {
+        console.log('Current images:', images); // This will log the updated state after changes
+    }, [images]);
 
     return (
         <div>
-            <div className="ImageCarousel w-screen h-[80vh] bg-gray-200 flex justify-center items-center" data-carousel="slide"
-                style={{backgroundImage: `url(${imageUrls[0]})`}}>
-                
+            <div className="ImageCarousel w-screen h-[80vh] bg-gray-200 flex justify-center items-center">
+                {images.map((image, index) => (
+                    <img key={index} src={image.img_src} alt="Display Image" className="w-full h-auto" />
+                ))}
             </div>
-                <div className="bg-azure bg-opacity-50 p-9 scale-75 md:scale-100 xl:scale-125 text-center">
-                    <div className="text-white text-2xl font-bold font-['Arial'] leading-9">ALPHA TAU OMEGA<br/>AT</div>
-                    <img className="w-60" src='/src/assets/CSMLogoW.png'/>
-                </div>
+            <div className="bg-azure bg-opacity-50 p-9 scale-75 md:scale-100 xl:scale-125 text-center">
+                <div className="text-white text-2xl font-bold font-['Arial'] leading-9">ALPHA TAU OMEGA<br/>AT</div>
+                <img className="w-60" src='/src/assets/CSMLogoW.png'/>
+            </div>
         </div>
-        
     );
 }
-export default HomePage
 
+export default HomePage;
---
+++ ato-ea-site/src/utils/supabaseClient.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+import { Database } from './supabaseTypes'
+
+const supabaseUrl = "https://yzkxinxskwwhigovfixx.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6a3hpbnhza3d3aGlnb3ZmaXh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkxMDcwMzIsImV4cCI6MjAzNDY4MzAzMn0.z3veH7-Vob5QPNXzJghjRFMGy3fQs8gn5dunciWGX1A"
+
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey)
+
+console.log('Supabase client created:', supabase);
