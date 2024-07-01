2e2614a0e71c5bdc6eeb1ae8b3e36a8c98bad1ca
Wesley Hu <whu@mines.edu>
2024-07-01 13:22:07 -0600

    added fade in on scroll

--- ato-ea-site/src/pages/HomePage.tsx
+++ ato-ea-site/src/pages/HomePage.tsx
@@ -3,6 +3,8 @@ import { supabase } from '../utils/supabaseClient';
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
 import { FaInstagram } from "react-icons/fa";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 //Define types for Images
 type ImageItem = {
@@ -68,11 +70,12 @@ function HomePage() {
         }
     }
     useEffect(() => {
-        console.log(exec);
-    }, [exec]);
-    useEffect(() => {
         fetchImages();
         fetchExec();
+        AOS.init({
+            duration: 1000,
+            once: true, // Ensures the animation happens only once
+        });
     }, []);
 
     // Carousel functions
@@ -93,7 +96,7 @@ function HomePage() {
     useEffect(() => {
         const interval = setInterval(() => {
             nextSlide();
-        }, 5000); // Change slide every 5 seconds
+        }, 5000); //Change slide every 5 seconds
 
         return () => clearInterval(interval);
     }, [currentIndex]);
@@ -101,6 +104,7 @@ function HomePage() {
     if (isLoading) {
         return <div className="w-screen h-screen flex items-center justify-center"></div>;
     }
+
 
     return (
         <div id="HomepageContainer" className="flex flex-col justify-center items-center">
@@ -128,7 +132,7 @@ function HomePage() {
                 </div>
             </div>
 
-            <div id="ChapterValues" className="w-full h-auto lg:scale-100 px-3 mb-10 lg:mb-20 flex flex-col justify-center items-center text-center">
+            <div data-aos="fade-up" id="ChapterValues" className="w-full h-auto lg:scale-100 px-3 mb-10 lg:mb-20 flex flex-col justify-center items-center text-center">
                 <h1 className="mb-10 text-black text-4xl md:text-5xl font-bold font-['Arial'] leading-normal">Welcome to the Epsilon Alpha Chapter</h1>
                 <p className=" lg:w-[65%] mb-10 text-center text-black text-xl font-medium font-['Arial'] leading-normal">The Alpha Tau Omega fraternity is a tight-knit community dedicated to fostering strong bonds of brotherhood and cultivating leadership skills among its members. With a rich history rooted in tradition and a commitment to service, ATO provides a supportive environment where young men can grow personally and professionally. Through a variety of social, philanthropic, and educational activities, ATO empowers its brothers to become confident leaders who make a positive impact in their communities and beyond. Joining ATO means joining a lifelong brotherhood built on friendship, integrity, and a shared vision for a brighter future.</p>
                 <a href="/values" className="w-44 h-14 flex justify-center items-center text-white bg-azure rounded-full transition-all duration-300 hover:bg-dark-blue group hover:text-old-gold">
@@ -136,7 +140,7 @@ function HomePage() {
                 </a>
             </div>
 
-            <div id="AboutUs" className="w-full 2xl:max-w-[80%] mb-10 lg:mb-20 h-auto px-3 md:px-10 flex flex-col-reverse lg:flex-row gap-0 lg:gap-5 relative">
+            <div data-aos="fade-up" id="AboutUs" className="w-full 2xl:max-w-[80%] mb-10 lg:mb-20 h-auto px-3 md:px-10 flex flex-col-reverse lg:flex-row gap-0 lg:gap-5 relative">
                 <div className="w-full lg:w-3/5 h-auto p-5 flex flex-col bg-azure rounded-br-3xl rounded-bl-3xl rounded-tl-none lg:rounded-tl-3xl rounded-tr-none lg:rounded-3xl">
                     <div className="mb-5 text-white text-3xl font-bold font-['Arial'] leading-10">Who We Are</div>
                     <div className="mb-5 text-white text-xl font-medium font-['Arial'] leading-normal">Founded on September 11, 1865, at the Virginia Military Institute, Alpha Tau Omega (ATO) was established by Otis Allan Glazebrook, Erskine Mayo Ross, and Alfred Marshall to promote unity and brotherhood after the Civil War. ATO was the first fraternity founded as a national organization, emphasizing leadership, scholarship, and service.<br/><br/>The Epsilon Alpha chapter was established in 1929 at the Colorado School of Mines. Dedicated to the fraternity's core values, the chapter has built a reputation for academic excellence, leadership, and community service. Over the years, Epsilon Alpha has actively engaged in philanthropic activities and campus involvement, fostering a strong sense of brotherhood and making a positive impact on both the university and the Golden, Colorado community.</div>
@@ -149,7 +153,7 @@ function HomePage() {
                 <div className="w-full lg:w-2/5 h-52 md:h-96 lg:h-auto rounded-tl-3xl rounded-tr-3xl rounded-bl-none lg:rounded-bl-3xl rounded-br-none lg:rounded-br-3xl" style={{backgroundImage: `url("src/assets/ATOold.jpg")`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center"}}></div>
             </div>
 
-            <div id="Philo" className="w-full 2xl:max-w-[80%] mb-10 lg:mb-20 h-auto px-3 md:px-10 flex flex-col lg:flex-row gap-0 lg:gap-5 relative">
+            <div data-aos="fade-up" id="Philo" className="w-full 2xl:max-w-[80%] mb-10 lg:mb-20 h-auto px-3 md:px-10 flex flex-col lg:flex-row gap-0 lg:gap-5 relative">
                 <div className="w-full lg:w-[45%] h-52 md:h-96 lg:h-auto rounded-tl-3xl rounded-tr-3xl rounded-bl-none lg:rounded-bl-3xl rounded-br-none lg:rounded-br-3xl" style={{backgroundImage: `url("src/assets/HomepagePhilo.jpeg")`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center"}}></div>
                 <div className="w-full lg:w-[55%] h-auto p-5 flex flex-col bg-azure rounded-br-3xl rounded-bl-3xl rounded-tl-none lg:rounded-tl-3xl rounded-tr-none lg:rounded-3xl">
                     <div className="mb-5 text-white text-3xl font-bold font-['Arial'] leading-10">Philanthropy and Service</div>
@@ -162,14 +166,14 @@ function HomePage() {
                 </div>
             </div>
 
-            <div id="JoinUs" className="w-screen mb-20 flex flex-col items-center justify-center lg:gap-14 py-3 lg:py-14 lg:px-20 bg-azure">
+            <div data-aos="fade-up" id="JoinUs" className="w-screen mb-20 flex flex-col items-center justify-center lg:gap-14 py-3 lg:py-14 lg:px-20 bg-azure">
                 <div className="text-white text-center text-4xl lg:text-5xl font-bold font-['Arial'] leading-normal">Interested in Joining ?</div>
                 <a href="/how-to-join" className="mb-5 lg:mb-0 w-80 h-24 scale-75 lg:scale-100 bg-old-gold rounded-full flex items-center justify-center transition-all duration-300 hover:bg-dark-gold hover:text-neutral-700">
                     <div className="text-black text-4xl font-bold font-['Arial'] leading-10">Start Here</div>
                 </a>
             </div>
 
-            <div id="Contact" className="w-full 2xl:max-w-[60%] h-auto px-0 lg:px-10 flex flex-col lg:flex-row gap-0 lg:gap-5 justify-center">
+            <div data-aos="fade-up" id="Contact" className="w-full 2xl:max-w-[60%] h-auto px-0 lg:px-10 flex flex-col lg:flex-row gap-0 lg:gap-5 justify-center">
                 <div className="w-full py-10 px-5 lg:px-20 rounded-none lg:rounded-3xl flex flex-col justify-center items-center bg-azure">
                     <div className="mb-10 text-white text-center text-4xl lg:text-5xl font-bold font-['Arial'] leading-normal">Get in Contact</div>
 
