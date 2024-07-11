f6f50a39bfc7be881d0715771e607c2ce5885699
Wesley Hu <whu@mines.edu>
2024-07-10 20:13:33 -0600

    working on cv page, got some animations working

--- ato-ea-site/src/pages/ChapterValuesPage.tsx
+++ ato-ea-site/src/pages/ChapterValuesPage.tsx
@@ -1,10 +1,10 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect, useRef} from 'react';
 import Sticky from 'react-stickynode';
 import ScrollSpy from 'react-ui-scrollspy';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { FaUsers } from "react-icons/fa";
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence, useInView } from 'framer-motion';
 
 
 function ChapterValuesPage() {
@@ -34,12 +34,15 @@ function ChapterValuesPage() {
         };
     }, []);
 
+    const refs = Array.from({ length: 7 }, () => useRef(null));
+    const inViews = refs.map(ref => useInView(ref, { once: false }));
+
     const handleSmoothScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
         e.preventDefault();
         const targetId = e.currentTarget.getAttribute('href')?.split('#')[1];
         const targetElement = targetId ? document.getElementById(targetId) : null;
         if (targetElement) {
-          const headerOffset = 144;
+          const headerOffset = 140;
           const elementPosition = targetElement.getBoundingClientRect().top;
           const offsetPosition = elementPosition + window.scrollY - headerOffset;
     
@@ -52,8 +55,8 @@ function ChapterValuesPage() {
 
     useEffect(() => {
         AOS.init({
-            duration: 1500,
-            offset: 500,
+            duration: 1000,
+            offset: 100,
             once: true, // Ensures the animation happens only once
         });
     }, []);
@@ -69,7 +72,7 @@ function ChapterValuesPage() {
             </div>
             <div id="SideBar" className="w-screen 2xl:w-3/5 px-3 mt-10 md:px-10 flex flex-col md:flex-row justify-center items-center md:items-start gap-10 md:gap-2 mb-10">
                 <div className="w-screen hidden md:w-[25%] md:flex flex-col">
-                    <Sticky enabled={true} top={144}>
+                    <Sticky enabled={true} top={140}>
                         <div data-aos="fade-right" className="w-4/5 flex flex-col gap-3">
                             <ScrollSpy>
                                 <ul className="nav-list flex flex-col gap-3">
@@ -103,18 +106,31 @@ function ChapterValuesPage() {
                         </div>
                     </Sticky>
                 </div>
-                <div className="w-screen md:w-full px-3 md:px-0 flex flex-col gap-20">
-                    <div data-aos="fade-up" id="TheCreed" className="bg-azure p-3 md:p-8 lg:p-14 flex flex-col justify-center items-start rounded-3xl">
+                <div className="w-[75%] md:w-full px-3 md:px-0 flex flex-col gap-20">
+                    <div data-aos="zoom-in"  id="TheCreed" className="bg-azure p-3 md:p-8 lg:p-14 flex flex-col justify-center items-start rounded-3xl">
                         <h1 className="text-white text-5xl font-bold mb-10">The Creed of Alpha Tau Omega</h1>
                         <p className=" text-white text-xl font-medium leading-loose">To bind men together in a brotherhood based upon eternal and immutable principles, with a bond as strong as right itself and as lasting as humanity; to know no North, no South, no East, no West, but to know man as man, to teach that true men the world over should stand together and contend for supremacy of good over evil; to teach, not politics, but morals; to foster, not partisanship, but the recognition of true merit wherever found; to have no narrower limits within which to work together for the elevation of man than the outlines of the world: these were the thoughts and hopes uppermost in the minds of the founders of the Alpha Tau Omega Fraternity.<br/><br/>-Otis Allan Glazebrook<br/>1880</p>
                     </div>
-                    <div data-aos="fade-left" id="OurValues" className="flex flex-col justify-center items-start gap-5">
-                        <h1 className="text-5xl font-bold mb-5">Our Values</h1>
-                        <p className="text-xl font-medium leading-loose">At the Epsilon Alpha chapter of Alpha Tau Omega, our core values are the foundation of our brotherhood. They guide our actions, shape our community, and inspire us to make a positive impact. Here are the values that define who we are:</p>
-                        <div className='flex flex-col md:flex-row'>
+                    <div id="OurValues" className="flex flex-col justify-center items-start gap-5">
+                        <div data-aos="zoom-in">
+                            <h1 className="text-5xl font-bold mb-5">Our Values</h1>
+                            <p className="text-xl font-medium leading-loose">At the Epsilon Alpha chapter of Alpha Tau Omega, our core values are the foundation of our brotherhood. They guide our actions, shape our community, and inspire us to make a positive impact. Here are the values that define who we are:</p>
                         </div>
+                        
+                        <div className="w-full flex flex-col md:flex-row justify-start items-start">
+                            <div className="w-40 h-40 bg-old-gold rounded-tl-3xl rounded-bl-3xl"></div>
+                            <motion.div ref={refs[0]} initial={{ scaleX: 0 }} animate={{ scaleX: inViews[0] ? 1 : 0 }} transition={{ duration: 1 }} className="w-4/5 h-40 bg-azure rounded-tr-3xl rounded-br-3xl origin-left">
+                            </motion.div>
+                        </div>
+                        <div className="w-full flex flex-col md:flex-row justify-start items-start">
+                            <motion.div ref={refs[1]} initial={{ scaleX: 0 }} animate={{ scaleX: inViews[1] ? 1 : 0 }} transition={{ duration: 1 }} className="w-4/5 h-40 bg-azure rounded-tl-3xl rounded-bl-3xl origin-right">
+                            </motion.div>
+                            <div className="w-40 h-40 bg-old-gold rounded-tr-3xl rounded-br-3xl"></div>
+                        </div>
+                        
+
                     </div>
-                    <div data-aos="fade-up" id="DEIA" className="bg-azure mb-96 p-3 md:p-8 lg:p-14 flex flex-col justify-center items-start rounded-3xl">
+                    <div data-aos="zoom-in" id="DEIA" className="bg-azure mb-96 p-3 md:p-8 lg:p-14 flex flex-col justify-center items-start rounded-3xl">
                         <h1 className="text-white text-3xl md:text-4xl lg:text-5xl font-bold mb-10">Diversity, Equity, Inclusion, and Accessibility Position statement</h1>
                         <div className="flex flex-col lg:flex-row justify-center text-white items-center gap-10">
                             <FaUsers className='w-full md:w-1/4' size={100}/>
