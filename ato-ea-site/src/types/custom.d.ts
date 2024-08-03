517f6e3911d77c79ce8842292414f574ca4b9ed3
Wesley Hu <whu@mines.edu>
2024-08-03 11:54:54 -0600

    trieed cusom types

---
+++ ato-ea-site/src/types/custom.d.ts
@@ -0,0 +1,14 @@
+declare global {
+    namespace MyCustomTypes {
+      declare module 'react-ui-scrollspy';
+      declare module 'emailjs-com';
+      declare module '@supabase/supabase-js';
+      declare module 'framer-motion';
+      declare module 'date-fns';
+      declare module 'react-input-mask';
+      declare module 'react-parallax';
+    }
+  }
+  
+  export {};
+  
