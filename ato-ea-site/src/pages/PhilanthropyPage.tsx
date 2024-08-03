895baf542616489a923c2cecdfd12c3a754aae49
Wesley Hu <whu@mines.edu>
2024-08-03 11:45:06 -0600

    one more change

--- ato-ea-site/src/pages/PhilanthropyPage.tsx
+++ ato-ea-site/src/pages/PhilanthropyPage.tsx
@@ -66,7 +66,7 @@ function PhilanthropyPage() {
     return (
         <div id="ChapterValues" className="flex flex-col justify-center items-center">
             <div id="Image" className="w-screen h-[60vh] mb-10 lg:mb-16 md:h-[80vh] relative bg-azure group">
-                <div className="w-full h-full bg-center bg-cover duration-500 flex justify-center items-center bg-[url(src/assets/philo/Philo.jpg)]">
+                <div className="w-full h-full bg-center bg-cover duration-500 flex justify-center items-center bg-philo">
                     <div className="bg-azure bg-opacity-50 py-16 px-9 scale-75 md:scale-100 lg:scale-125 xl:scale-150 text-center">
                         <div className="text-white text-4xl font-bold leading-9">Philanthropy</div>
                     </div>
