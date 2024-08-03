cfa6faece4854b786d4f7726245ddf538cfc9aa0
Wesley Hu <whu@mines.edu>
2024-08-03 12:24:55 -0600

    email.js

--- ato-ea-site/vite.config.ts
+++ ato-ea-site/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['framer-motion', 'date-fns', 'react-input-mask']
+      external: ['framer-motion', 'date-fns', 'react-input-mask', 'emailjs-com']
     }
   }
 })
